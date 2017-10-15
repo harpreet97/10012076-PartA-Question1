1e68ab357f5a2d686376b0ea3f085792a2cbbdc0
harpreet97 <harrycheema97@yahoo.com>
2017-10-15 14:01:50 +1300

    initial commit

--- script.js
+++ script.js
@@ -5,33 +5,15 @@ let pw = document.querySelector('#pw')
 let pw2 = document.querySelector('#pw2')
 let submit = document.querySelector('#submit')
 
-
-// This functions is used to run control events
-$(document).ready(function () {
-
-    //Step 1:
-    alert("Welcome");
-
-    //Step 2:
-    $("#submit").click(function () {
-        if (un.value == "" || pw.value == "" || pw2.value == "") {
-            alert("Please fill the Username and Password fields first.");
-            e.preventDefault();
-        }
-        else {
-            submitFunction();
-        }
-    });
-
-})
+alert("Welcome");
 
 // Checking the username should be at least 8 characters.
 function CheckUsernameLength() {
-    if (un.value.length <= 8) {
+    if (un.value.length >= 8) {
         return true;
     }
-    else{
-        alert("Make sure your Username is less than 8 characters.");
+    else {
+        alert("Make sure your Username is more than 8 characters.");
         return false;
     }
 }
@@ -54,3 +36,16 @@ function submitFunction() {
         }
     }
 }
+
+// This functions is used to run control events
+submit.addEventListener("click", function () {
+
+    if (un.value == "" || pw.value == "" || pw2.value == "") {
+        alert("Please fill the Username and Password fields first.");
+    }
+    else {
+        submitFunction();
+    }
+
+
+})
